e82e3dedeb4e78ae646ba37349d08d7df1fb5f9a
zinnli <limgx5192@gmail.com>
2024-04-21 23:54:26 +0900

    feat: gemotion declare 설정 및 path 이슈로 packages 추가 (#5)

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,18 @@
-import { useState } from 'react'
-import './App.css'
-import { ReactIcon } from './assets'
+import { ThemeProvider } from "@emotion/react"
+import { ReactIcon } from "assets"
+import { Input } from "components"
+import { theme } from "styles/theme"
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
+    <div>
+      <ThemeProvider theme={theme}>
           <ReactIcon/>
-        </a>
-        <a href="https://react.dev" target="_blank">
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+          <Input/>
+    </ThemeProvider>
+    </div>
   )
 }
 
---
+++ src/components/common/input/Input.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import * as S from "./Input.styled"
+
+export default function Input() {
+  return (
+	<S.Input />
+  )
+}
