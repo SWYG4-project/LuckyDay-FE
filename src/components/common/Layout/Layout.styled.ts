e452a0f5ab08c4c83d54661e37f65fc947cc57a4
yrk721 <yoorae0721@naver.com>
2024-04-29 00:25:54 +0900

    feat: Layout에 background image 삽입, 좌표설정 (#10)

--- src/components/common/Layout/Layout.styled.ts
+++ src/components/common/Layout/Layout.styled.ts
@@ -2,11 +2,15 @@ import styled from "@emotion/styled";
 import { css } from "@emotion/react";
 
 export const Layout = styled.div`
-  ${({ theme }) => css`
+  ${() => css`
     width: 430px;
     height: 932px;
-    margin: 0 auto;
-    border: 10px solid ${theme.colors.purple};
-    background-color: ${theme.colors.gray};
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%); // 추후에 상대좌표로 변경할 수 있음
+    background-image: url("src/assets/background.png");
+    background-size: cover;
+    background-position: center;
   `}
 `;
