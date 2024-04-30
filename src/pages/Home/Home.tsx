ca1df261036ab8d720d19298d8bff13b2d81c507
zinnli <limgx5192@gmail.com>
2024-04-30 20:09:07 +0900

    chore: 불필요한 코드 및 파일 삭제 (#11)

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,5 @@
-import { Input } from "components";
-import { MenuIcon } from "assets";
-import * as S from "./Home.styled";
+import { Calendar } from "components";
 
 export default function Home() {
-  return (
-    <S.Home>
-      <Input placeholder="input 칸 입니다" />
-      <MenuIcon css={S.menuIcon} />
-    </S.Home>
-  );
+  return <Calendar />;
 }
