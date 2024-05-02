7020d6cfc3e512c8a5f5bc4ada4607ea6617cc75
zinnli <limgx5192@gmail.com>
2024-05-02 23:39:34 +0900

    feat: 제외 날짜 선택 스크린 제작(#16)

--- src/pages/createLuckyDay/selectExceptDay/SelectExceptDay.tsx
+++ src/pages/createLuckyDay/selectExceptDay/SelectExceptDay.tsx
@@ -1,7 +1,26 @@
 import React from "react";
 
+import { Calendar } from "components";
+import * as S from "./SelectExecptDay.styled";
+import dayjs from "dayjs";
+
 function SelectExceptDay() {
-  return <div>SelectExceptDay</div>;
+  const selectedPeriod = "30";
+  const EndOfDate = dayjs(dayjs())
+    .add(+selectedPeriod, "day")
+    .format("YYYY년 MM월 DD일");
+
+  return (
+    <>
+      <S.HeadLine>럭키 데이 배정을 원하지 않는 날짜를 선택하세요.</S.HeadLine>
+      <S.SubHeadLine>
+        {dayjs().format("YYYY년 MM월 DD일")} ~ {EndOfDate}
+      </S.SubHeadLine>
+      <div>
+        <Calendar dates={selectedPeriod} />
+      </div>
+    </>
+  );
 }
 
 export default SelectExceptDay;
