c68f0e491c79045e53cda17ee4929f3fed944acf
yrk721 <yoorae0721@naver.com>
2024-05-03 04:23:23 +0900

    feat: luckyBoard page 생성 및 UI 제작 (#19)

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,3 @@
-export { default as Landing } from "./landing/Landing";
+export { default as LandingPage } from "./landing/Landing";
+export { default as LuckyBoardPage } from "./luckyBoard/LuckyBoard";
 export { default as Error404Page } from "./404/404";
