ffdf1ee602010181244b926dec11d297ea4a126e
yrk721 <yoorae0721@naver.com>
2024-05-11 05:15:58 +0900

    feat: 프로필 변경 페이지 UI 제작 (#44)

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,6 +2,7 @@ export { default as LandingPage } from "./landing/LandingPage";
 export { default as Auth } from "./auth/Auth";
 export { default as Profile } from "./profile/Profile";
 export { default as MyPage } from "./myPage/MyPage";
+export { default as EditProfilePage } from "./editProfile/EditProfilePage";
 export { default as LuckyBoardPage } from "./luckyBoard/LuckyBoardPage";
 export { default as CreateLuckyDayPage } from "./createLuckyDay/CreateLuckyDayPage";
 export { default as SettingLuckyBoardPage } from "./settingLuckyBoard/SettingLuckyBoardPage";
