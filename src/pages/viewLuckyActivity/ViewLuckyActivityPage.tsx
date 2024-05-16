629328fd9ca7aa6e75bdf6ae741c39c31fe0e18e
zinnli <limgx5192@gmail.com>
2024-05-16 23:50:42 +0900

    feat: 럭키데이 열람- 추억,오늘 스크린에 api 연동 (#69)

--- src/pages/viewLuckyActivity/ViewLuckyActivityPage.tsx
+++ src/pages/viewLuckyActivity/ViewLuckyActivityPage.tsx
@@ -1,42 +1,50 @@
 import dayjs from "dayjs";
+import { useNavigate, useParams } from "react-router-dom";
 
 import { SingleButtonLayout, SvgFrame } from "components";
+import { useGetLuckyDayDetail } from "services";
 import { formatDate } from "utils";
 import { ShortBoxIcon, activities } from "assets";
 import * as S from "./ViewLuckyActivityPage.styled";
 
 function ViewLuckyActivityPage() {
-  const data = {
-    actNm: "향초로 기분 전환하기",
-    actInfo:
-      "나에게 향초를 선물해서 분위기를 내 보는 것은 어떨까요? 지친 하루를 마무리할 때, 내가 직접 고른 향이 나는 향초는 편안한 휴식과 아늑한 밤을 선사해 줄 거예요.",
-    review: null,
-    imageName: null,
-    imagePath: null,
-    imageUrl: null,
-    dday: "2024-05-16",
-    category: "특별한 선물",
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const { data } = useGetLuckyDayDetail(id!);
+
+  const handleClickRecord = () => {
+    if (data?.resData.review) {
+      navigate("/"); //TODO: 적절한 경로로 변경 : 상세 보기
+    } else {
+      navigate("/luckyboard"); //TODO: 적절한 경로로 변경 : 기록 남기기
+    }
   };
 
   return (
     <SingleButtonLayout>
       <S.ViewLuckyActivityPage>
         <S.LuckydayInfo>
-          <span>{formatDate(data.dday)}</span>
+          <span>{formatDate(data?.resData.dday)}</span>
           <span>
-            {data.dday === dayjs().format("YYYY-MM-DD") ? "오늘의" : "추억의"}{" "}
+            {data?.resData.dday === dayjs().format("YYYY-MM-DD")
+              ? "오늘의"
+              : "추억의"}{" "}
             럭키 데이 활동은...
           </span>
         </S.LuckydayInfo>
         <S.LuckydayDetailInfo>
-          <S.Img src="public/images/img_luckydayBg.png" />
-          {activities.find((item) => item.label === data.category)?.icon}
-          <p>{data.actNm}</p>
-          <p>{data.actInfo}</p>
+          <S.Img src="/public/images/img_luckydayBg.png" alt="luckyday" />
+          {
+            activities.find((item) => item.label === data?.resData.category)
+              ?.icon
+          }
+          <p>{data?.resData.actNm}</p>
+          <p>{data?.resData.actInfo}</p>
         </S.LuckydayDetailInfo>
-        <S.Button>
+        <S.Button onClick={handleClickRecord}>
           <SvgFrame css={S.svgFrame} icon={<ShortBoxIcon />} />
-          <span>{data.review ? "기록하기" : "기록보기"}</span>
+          <span>{data?.resData.review ? "기록하기" : "기록보기"}</span>
         </S.Button>
       </S.ViewLuckyActivityPage>
     </SingleButtonLayout>
