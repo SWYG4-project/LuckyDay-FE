68dc089d5fca94bb24cd92c5d1630245839c63e5
yrk721 <yoorae0721@naver.com>
2024-05-18 05:08:58 +0900

    feat: 럭키보드 푸터 버튼 추가, 아카이브 모달 연결, 토스트 추가 (#72)

--- src/pages/luckyBoard/luckyBoardAfter/LuckyBoardAfterPage.tsx
+++ src/pages/luckyBoard/luckyBoardAfter/LuckyBoardAfterPage.tsx
@@ -1,9 +1,18 @@
 import * as S from "./LuckyBoardAfterPage.styled";
+import { useModal } from "hooks";
 import { getCurrentDate } from "utils/date";
-import { ButtonLayout, LuckyBalls } from "components";
+import { ArchiveModal, ButtonLayout, LuckyBalls } from "components";
 
 export default function LuckyBoardAfterPage() {
+  const { handleOpenModal } = useModal();
   console.log(getCurrentDate());
+
+  const handleClickFirstButton = () => {
+    handleOpenModal(<ArchiveModal />);
+  };
+
+  const handleSecondButtonClick = () => {};
+
   return (
     <>
       {/* FIX: API 연결 예정입니다. */}
@@ -11,6 +20,8 @@ export default function LuckyBoardAfterPage() {
         variant="hasColor"
         firstLabel={"지난 럭키데이"}
         secondLabel={"더보기"}
+        handleClickFirstButton={handleClickFirstButton}
+        handleClickSecondButton={handleSecondButtonClick}
       >
         <S.Container>
           <S.TextBox>{getCurrentDate()}</S.TextBox>
