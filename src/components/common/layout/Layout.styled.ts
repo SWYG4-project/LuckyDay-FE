1bec91fb8c22d04a79803ce90726129ab6f86c52
yrk721 <yoorae0721@naver.com>
2024-05-29 00:22:06 +0900

    fix: 생성 모달 UI 깨짐 이슈로 인한 코드 원복 (#117)

--- src/components/common/layout/Layout.styled.ts
+++ src/components/common/layout/Layout.styled.ts
@@ -20,6 +20,10 @@ export const Layout = styled.div`
   max-width: 430px;
   width: 100%;
   height: 100%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%); // FIX : 생성 모달 이슈로 인한 재수정
   background-size: cover;
   background-position: center;
   background-image: url("/images/background.webp");
